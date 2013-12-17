318a74a47669b261109a678ea90de73b349f305d
tobias.nickel <ross40k@googlemail.com>
2013-12-17 11:00:20 +0100

    improvement by switching to better oop with prototyping it saves a lot of memory and works much faster

--- tobserver.js
+++ tobserver.js
@@ -1,18 +1,41 @@
-function tobservable(pData, pObserverTree, pPath) {
-	"use strict";
-	if (!pData) 
-		pData = {};
-	if (!pObserverTree) 
-		pObserverTree = new observerTree();
-	if (!pPath) 
-		pPath = '';
+/**
+ * @fileOverview
+ * @author Tobias Nickel
+ * @bersion 0.03
+ */
 
-	var that = this;
-	this.data = pData; // the ProgrammData
-	this.observer = pObserverTree;
-	this.path = pPath;
+/**
+ * @class
+ * tobservable Class that handle the data
+ * @param {mixed} pData the data to observe
+ * @param {tobservable} pObserverTree the root observertree (used internally)
+ * @param {String} pPath the path of the data that is the current observer is careing about
+ */
+ var tobservable=( function(){
+	 function tobservable(pData, pObserverTree, pPath) {
+		"use strict";
+		if (!pData) 
+			pData = {};
+		if (!pObserverTree) 
+			pObserverTree = new this.observerTree();
+		if (!pPath) 
+			pPath = '';
 
-	this.get = function(pPath) {
+		var that = this;
+		this.data = pData; // the ProgrammData
+		this.observer = pObserverTree;
+		this.path = pPath;
+
+		
+	}
+	/**
+	 * will return a tObservable, that observes the given path
+	 * 
+	 * @param {type} pPath 
+	 *      identifies the path, that you want to have
+	 * @returns {tobservable}
+	 */
+	tobservable.prototype.get = function(pPath) {
 		var pathParts = (pPath + '').split('.');
 		if (pathParts.length === 0)
 			return this;
@@ -20,177 +43,149 @@ function tobservable(pData, pObserverTree, pPath) {
 			var name = pathParts[0];
 			if (pathParts.length === 1) {
 				return new tobservable(
-					that.data[name],
-					that.observer,
-					addNameToPath(that.path, name)
+					this.data[name],
+					this.observer,
+					this.addNameToPath(this.path, name)
 				);
 			} else {
 				pathParts.splice(0, 1);
-				return new tobservable(that.data[name], that.observer, addNameToPath(that.path, name), that.rootTobservable).get(mergeToPath(pathParts));
+				return new tobservable(this.data[name], this.observer, this.addNameToPath(this.path, name), this.rootTobservable).get(mergeToPath(pathParts));
 			}
 		}
 	};
-	this.set = function(pPath, value, run) {
+	
+	
+	/**
+	 * used to set a value on the data, it will notify the observer
+	 * @param {String} pPath
+	 *      identifing the data to update
+	 * @param {Mixed} value
+	 *      the new value of the described data
+	 * @param {Boolean} run
+	 *      
+	 * @returns {void}
+	 */
+	tobservable.prototype.set = function(pPath, value, run) {
 		var pathParts = (pPath + '').split('.');
 		var name = pPath;
 		if (pathParts.length === 0)
 			return null;
 		if (pathParts.length === 1) {
 			if (run) {
-				var out = that.data[name].apply(that.data, value);
-				that.notify(name);
+				var out = this.data[name].apply(this.data, value);
+				this.notify(name);
 				return out;
 			} else {
-				that.data[name] = value;
-				that.notify(name);
+				this.data[name] = value;
+				this.notify(name);
 				return this;
 			}
 		} else {
 			name = pathParts.pop();
-			that.get(mergeToPath(pathParts)).set(name, value, run);
+			this.get(mergeToPath(pathParts)).set(name, value, run);
 		}
 	};
-	this.run = function(pPath, value) {
-		that.set(pPath, value, true);
+	
+	/**
+	 * will apply a function described with the path,
+	 * @param {String} pPath
+	 *      describs the Data-Path
+	 * @param {Array} value
+	 *      the Parameterlist for the given function 
+	 * @returns {void}
+	 */
+	tobservable.prototype.run = function(pPath, value) {
+		this.set(pPath, value, true);
 	};
-	this.registerObserver = function(tObserver, pPath) {
-		that.observer.addListener(tObserver, pPath);
-	};
-	//remove the listener on the given path, where the last value is the name property of the observable, 
-	//if it is not existing, it will remove nothing.
-	this.removeObserver = function(path) {
-		that.observer.removeListener(path);
-	};
-
-	this.notify = function(path) {
-		that.observer.runUpdate(addNameToPath(that.path, path));
-	};
- 
-	function addNameToPath(path, name) {
+	/**
+	 * @private
+	 * @param {String} path
+	 *      data Path as known
+	 * @param {type} name
+	 *      the name to add
+	 * @returns {String}
+	 */
+	tobservable.prototype.addNameToPath=function (path, name) {
 		return path===''?name:path+'.'+name;
 	}
 
-	// merges a array of names to a path
-	function mergeToPath(array) {
-		var out = '';
-		for (var ii = 0; ii < array.length; ii++) 
-			if (out === '') 
-				out += array[ii];
-			else 
-				out += '.' + array[ii];
-		return out;
-	}
-	//---------------------------------------
-	// subclass observerTree
-	//---------------------------------------
-	function observerTree(pObserver, pNextPath) {
-		var that = this;
-		this.$listener = [];
-		//to add a Listener to the tree
-		this.addListener = function(pListener, pPath) {
-			var pathParts = pPath.split('.');
-			pathParts = removeEmptyStrings(pathParts);
-			if (pathParts.length > 0) {
-				var prop = toProertyname(pathParts[0]);
-				pathParts.shift();
-				if (typeof(that[prop]) === "undefined")
-					that[prop] = new observerTree(pListener, mergeToPath(pathParts));
-				else 
-					that[prop].addListener(pListener, mergeToPath(pathParts));
-			} else
-				that.$listener.push(pListener);
-		};
-		// remove the Listener, that 
-		this.removeListener = function(pPath) {
-			if (typeof(pPath) === "undefined") 
-				pPath = '';
-			var pathParts = pPath.split('.');
-			pathParts = removeEmptyStrings(pathParts);
-			if (pathParts.length > 1) {
-				var PropName = toProertyname(pathParts[0]);
-				pathParts.shift();
-				if (typeof(that[PropName]) !== 'undefined')
-					that[PropName].removeListener( mergeToPath(pathParts));
-			} else 
-				if (pathParts.length === 1 && typeof(that[pathParts[0]]) !== 'undefined') 
-					for (var ii in that.$listener) 
-						if (typeof that.$listener[ii].name === 'string' && that.$listener[ii].name === PropName) 
-							that.$listener.splice(that.$listener[ii], 1);
-		};
+	/**
+	 * remove the described observer
+	 * @param {type} path
+	 *      tha data-path to the observer, where the last part is the name
+	 * @returns {void}
+	 */
+	tobservable.prototype.removeObserver = function(path) {
+		this.observer.removeListener(path);
+	};
 
-		this.runUpdate = function( pPath) {
-			if (typeof(pPath) === "undefined")
-				pPath = '';
-			for (var ii in that.$listener) 
-				that.$listener[ii].update();
-			var pathParts = removeEmptyStrings(pPath.split('.'));
-			if (pathParts.length > 0) {
-				var PropName = toProertyname(pathParts[0]);
-				if (typeof(that[PropName]) !== 'undefined') {
-					pathParts.splice(0, 1);//TODO
-					that[PropName].runUpdate( mergeToPath(pathParts));
-				}
-			}else
-				for(var index in that) if(index.indexOf('_t_')==0)
-					that[index].runUpdate("");
-		};
-		// merges a array of names to a path
-		function mergeToPath(array) {
-			var out = '';
-			for (var ii = 0; ii < array.length; ii++) 
-				out += out === '' ? array[ii] : '.'+array[ii];
-			return out;
-		}
-		//remove all "" strings
-		function removeEmptyStrings(array) {
-			while (array.indexOf('') !== -1) 
-				array.splice(array.indexOf(''), 1);
-			return array;
-		}
-		function toProertyname(name) {
-			return '_t_' + name;
-		}
-		//run initialisation of observertree
-		if (typeof(pNextPath) === "undefined") 
-			pNextPath = '';
-		
-		if (typeof(pObserver) !== "undefined")
-			if (pNextPath === '') 
-				this.$listener.push(pObserver);
-			else 
-				this.addListener(pObserver, pNextPath);
-	}
-	var stdClassName="TobservableView";
-	this.initDomViews=function(){
-		var elements=document.getElementsByClassName(stdClassName);
+	/**
+	 * used to tell all observer on the given path to update there view
+	 * @param {type} path
+	 * @returns {undefined}
+	 */
+	tobservable.prototype.notify = function(path) {
+		this.observer.runUpdate(this.addNameToPath(this.path, path));
+	};
+	
+	/**
+	 * used to register a opserver on the described path
+	 * (it is not calling the update merthod.)
+	 * @param {Observer} tObserver
+	 *      an object like: {name:"someName",update:function(){}}
+	 * @param {type} pPath
+	 *      describes the path wherer the observer should be registered
+	 * @returns {void}
+	 */
+	tobservable.prototype.registerObserver = function(tObserver, pPath) {
+		this.observer.addListener(tObserver, pPath);
+	};
+	
+	
+	/**
+	 * the name of the HTML-class for the elements that are going to became updated by std views
+	 * @type String
+	 */
+	tobservable.prototype.stdClassName="TobservableView";
+	
+	/**
+	 * get all HTML objects with the given klass, and makes a view with them.
+	 * if also register observer ther on the DOM to create StdViews for the elements that are new Created
+	 * @returns {undefined}
+	 */
+	tobservable.prototype.initDomViews=function(){
+		var elements=document.getElementsByClassName(model.stdClassName);
 		for(var i=0; i < elements.length; i++)
-			new stdElementView(elements[i],that);
+			new this.stdElementView(elements[i],this);
 		// liveupdate in the dom
 		document.addEventListener('DOMNodeInserted',function(ev){
 			var node=ev.srcElement;
-			if(hasClass(node, stdClassName) && typeof node._tName=="undefined" ){
-			
-				//console.log(node,"added");
-				new stdElementView(node,model);
-			}
+			if(hasClass(node, model.stdClassName) && typeof node._tName==="undefined" )
+				new model.stdElementView(node,model);
 		});
 		document.addEventListener('DOMNodeRemoved',function(ev){
 			var node=ev.srcElement;
-			if(hasClass(node, stdClassName) && typeof node._tName!=="undefined"){
+			if(hasClass(node, this.stdClassName) && typeof node._tName!=="undefined"){
 				//console.log(node,"removed");
 				var tPath=node.getAttribute("tPath");
-				tPath=tPath!=null?tPath:"";
+				tPath=tPath!==null?tPath:"";
 				model.removeObserver(tPath+"."+node._tName);
 			}
 		});			
 	}; 
-	function stdElementView(e,model){
+	/**
+	 * the stdView, that is used for document-nodes
+	 * @param {dom-node} e
+	 * @param {tobservable} model
+	 * @returns {tobservable.stdElementView}
+	 */
+	tobservable.prototype.stdElementView=function (e,model){
 		var tProp=e.getAttribute("tprop");
 		var tPath=e.getAttribute("tpath");
-		tPath=tPath!=null?tPath:"";
+		tPath=tPath!==null?tPath:"";
 		var tFilter=e.getAttribute("tfilter");
 		var defaultValue="";
-		if(tProp==null)
+		if(tProp===null)
 			defaultValue=e.innerHTML;
 		else 
 			defaultValue=e.getAttribute(tProp);
@@ -199,10 +194,11 @@ function tobservable(pData, pObserverTree, pPath) {
 		e._tName=name;
 		this.update=function(){
 			var v=model.get(tPath).data;
-			v= typeof v=="string"?v:defaultValue;
+			v= typeof v === "number"?v+"":v;
+			v= typeof v==="string"?v:defaultValue;
 			if(tFilter!==null)
 				v=eval('('+tFilter+'('+v+'))');
-			if(tProp==null)
+			if(tProp===null)
 				e.innerHTML=v;
 			else
 				e.setAttribute(tProp,v);
@@ -210,9 +206,143 @@ function tobservable(pData, pObserverTree, pPath) {
 		model.registerObserver(this,tPath);
 		this.update();
 	}
-}
+	/**
+	 * @class
+	 * @private
+	 * Only used by the tobservable to manage the observer
+	 * @param {tobserbable} pObserver
+	 * @param {String} pNextPath
+	 * @returns {tobservable.observerTree}
+	 */
+	tobservable.prototype.observerTree=(function(){
+		function observerTree(pObserver, pNextPath) {
+			var that = this;
+			this.$listener = [];
+			
+			
+			//run initialisation of observertree
+			if (typeof(pNextPath) === "undefined") 
+				pNextPath = '';
+			
+			if (typeof(pObserver) !== "undefined")
+				if (pNextPath === '') 
+					this.$listener.push(pObserver);
+				else 
+					this.addListener(pObserver, pNextPath);
+		}
+		//***********
+		// code here for observerTree
+		//***********
+		//to add a Listener to the tree
+		observerTree.prototype.addListener = function(pListener, pPath) {
+			var pathParts = pPath.split('.');
+			pathParts = this.removeEmptyStrings(pathParts);
+			if (pathParts.length > 0) {
+				var prop = this.toProertyname(pathParts[0]);
+				pathParts.shift();
+				if (typeof(this[prop]) === "undefined")
+					this[prop] = new observerTree(pListener, mergeToPath(pathParts));
+				else 
+					this[prop].addListener(pListener, mergeToPath(pathParts));
+			} else
+				this.$listener.push(pListener);
+		};
+		
+		/**
+		 * one of the magic functions. it is executing the updatefuntion of all 
+		 * relevant listeners. so, all listener that are registered along the path,
+		 * and all Listeners under the Path.
+		 * @param {type} pPath
+		 * @returns {undefined}
+		 */
+		observerTree.prototype.runUpdate = function(pPath) {
+			if (typeof(pPath) === "undefined")
+				pPath = '';
+			for (var ii in this.$listener) 
+				this.$listener[ii].update();
+			var pathParts = this.removeEmptyStrings(pPath.split('.'));
+			if (pathParts.length > 0) {
+				var PropName = this.toProertyname(pathParts[0]);
+				if (typeof(this[PropName]) !== 'undefined') {
+					pathParts.splice(0, 1);//TODO
+					this[PropName].runUpdate( mergeToPath(pathParts));
+				}
+			}else
+				for(var index in this) if(index.indexOf('_t_')===0)
+					this[index].runUpdate("");
+		};
+		
+		/**
+		 * is removing the described Listener
+		 * @param {type} pPath
+		 *      the path to the lsitener, where the last part is the name
+		 * @returns {void}
+		 */
+		observerTree.prototype.removeListener = function(pPath) {
+			if (typeof(pPath) === "undefined") 
+				pPath = '';
+			var pathParts = pPath.split('.');
+			pathParts = this.removeEmptyStrings(pathParts);
+			if (pathParts.length > 1) {
+				var PropName = this.toProertyname(pathParts[0]);
+				pathParts.shift();
+				if (typeof(this[PropName]) !== 'undefined')
+					this[PropName].removeListener( mergeToPath(pathParts));
+			} else 
+				if (pathParts.length === 1 && typeof(this[pathParts[0]]) !== 'undefined') 
+					for (var ii in this.$listener) 
+						if (typeof this.$listener[ii].name === 'string' && this.$listener[ii].name === PropName) 
+							this.$listener.splice(this.$listener[ii], 1);
+		};
+		//remove all "" strings
+		observerTree.prototype.removeEmptyStrings=function(array) {
+			while (array.indexOf('') !== -1) 
+				array.splice(array.indexOf(''), 1);
+			return array;
+		}
+		//remove all "" strings
+		observerTree.prototype.removeEmptyStrings=function(array) {
+			while (array.indexOf('') !== -1) 
+				array.splice(array.indexOf(''), 1);
+			return array;
+		}
+		
+		observerTree.prototype.toProertyname=function(name) {
+			return '_t_' + name;
+		}
+		return observerTree;
+	})();
+
+	return tobservable;
+})();
+/**
+ * a helper function that checks if the nodeelement has a given class
+ * @param {type} elem
+ *      the dom-Element
+ * @param {String} klass
+ *      name of the class
+ * @returns {Boolean}
+ */
 function hasClass( elem, klass ) {
      return (" " + elem.className + " " ).indexOf( " "+klass+" " ) > -1;
+}
+
+
+/**
+ * merges a array of names to a path
+ * @private
+ * @param {Array} array
+ *      an array containing strings with the names
+ * @returns {String}
+ */
+var mergeToPath=function(array) {
+	var out = '';
+	for (var ii = 0; ii < array.length; ii++) 
+		if (out === '') 
+			out += array[ii];
+		else 
+			out += '.' + array[ii];
+	return out;
 }
 var data = {};
 var model = new tobservable(data); ;
