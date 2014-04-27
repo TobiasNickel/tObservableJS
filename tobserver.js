edc97d746cdb8450e762ab782d0823c02eaf22f5
TobiasNickel <ross40k@googlemail.com>
2014-04-27 20:56:55 +0200

    added the history to utility, cleaned up the stuff from the global scope

--- tobserver.js
+++ tobserver.js
@@ -11,7 +11,7 @@
  * @param {tobservable} pObserverTree the root observertree (used internally)
  * @param {String} pPath the path of the data that is the current observer is careing about
  */
- var tobservable=( function(){
+ var tobserver=( function(){
 	 function tobservable(pData, pObserverTree, pPath) {
 		"use strict";
 		if (!pData) 
@@ -273,12 +273,12 @@
 	 */
 	tobservable.prototype.initDomViews=function(){
 		// liveupdate in the dom
-		window.bindEvent(document,'DOMNodeInserted',function(ev){
+		tobserver.utils.bindEvent(document,'DOMNodeInserted',function(ev){
 			var element=ev.srcElement;
 			new tobserver.findTObserver(element);
 		});
 		//document.addEventListener('DOMNodeInserted',);
-		window.bindEvent(document,'DOMNodeRemoved',function(ev){
+		tobserver.utils.bindEvent(document,'DOMNodeRemoved',function(ev){
 			if(ev.srcElement!=undefined&& ev.srcElement.getAttribute!=undefined)
 				setTimeout(function(){
 					//var element=ev.srcElement;
@@ -551,52 +551,370 @@
 					}
 				}else tobserver.notify(elementPath,round);
 			}
+		},
+		history:function(){
+			//NEEDED PARTS OF UNDERSCORE
+			var _ = {};
+			var	slice = Array.prototype.slice,
+				hasOwnProperty = Object.prototype.hasOwnProperty;
+
+			// All **ECMAScript 5** native function implementations that we hope to use
+			// are declared here.
+			var	nativeSome = Array.prototype.some,
+				nativeForEach = Array.prototype.forEach,
+				nativeBind = Function.prototype.bind;
+
+			// Create a function bound to a given object (assigning `this`, and arguments,
+			// optionally). Delegates to **ECMAScript 5**'s native `Function.bind` if
+			// available.
+			_.bind = function (func, context) {
+				var args, bound;
+				if (nativeBind && func.bind === nativeBind) return nativeBind.apply(func, slice.call(arguments, 1));
+				if (!_.isFunction(func)) throw new TypeError;
+				args = slice.call(arguments, 2);
+				return bound = function () {
+					if (!(this instanceof bound)) 
+						return func.apply(context, args.concat(slice.call(arguments)));
+					ctor.prototype = func.prototype;
+					var self = new ctor;
+					ctor.prototype = null;
+					var result = func.apply(self, args.concat(slice.call(arguments)));
+					if (Object(result) === result) return result;
+					return self;
+				};
+			};
+			// Bind a number of an object's methods to that object. Remaining arguments
+			// are the method names to be bound. Useful for ensuring that all callbacks
+			// defined on an object belong to it.
+			_.bindAll = function (obj) {
+				var funcs = slice.call(arguments, 1);
+				if (funcs.length === 0) throw new Error('bindAll must be passed function names');
+				each(funcs, function (f) {
+					obj[f] = _.bind(obj[f], obj);
+				});
+				return obj;
+			};
+			// The cornerstone, an `each` implementation, aka `forEach`.
+			// Handles objects with the built-in `forEach`, arrays, and raw objects.
+			// Delegates to **ECMAScript 5**'s native `forEach` if available.
+			var each = _.each = _.forEach = function (obj, iterator, context) {
+				if (obj == null) return obj;
+				if (nativeForEach && obj.forEach === nativeForEach) {
+					obj.forEach(iterator, context);
+				} else if (obj.length === +obj.length) {
+					for (var i = 0, length = obj.length; i < length; i++) {
+						if (iterator.call(context, obj[i], i, obj) === breaker) return;
+					}
+				} else {
+					var keys = _.keys(obj);
+					for (var i = 0, length = keys.length; i < length; i++) {
+						if (iterator.call(context, obj[keys[i]], keys[i], obj) === breaker) return;
+					}
+				}
+				return obj;
+			};
+
+			// Determine if at least one element in the object matches a truth test.
+			// Delegates to **ECMAScript 5**'s native `some` if available.
+			// Aliased as `any`.
+			var any = _.some = _.any = function (obj, predicate, context) {
+				predicate || (predicate = _.identity);
+				var result = false;
+				if (obj == null) return result;
+				if (nativeSome && obj.some === nativeSome) return obj.some(predicate, context);
+				each(obj, function (value, index, list) {
+					if (result || (result = predicate.call(context, value, index, list))) return breaker;
+				});
+				return !!result;
+			};
+			//UNDERSCORE ENDE
+
+			// Backbone.History
+			// ----------------
+
+			// Handles cross-browser history management, based on either
+			// [pushState](http://diveintohtml5.info/history.html) and real URLs, or
+			// [onhashchange](https://developer.mozilla.org/en-US/docs/DOM/window.onhashchange)
+			// and URL fragments. If the browser supports neither (old IE, natch),
+			// falls back to polling.
+			var History = function () {
+				//this.handlers = [];
+				_.bindAll(this, 'checkUrl');
+
+				// Ensure that `History` can be used outside of the browser.
+				if (typeof window !== 'undefined') {
+					this.location = window.location;
+					this.history = window.history;
+				}
+			};
+
+			// Cached regex for stripping a leading hash/slash and trailing space.
+			var routeStripper = /^[#\/]|\s+$/g;
+			// Cached regex for stripping leading and trailing slashes.
+			var rootStripper = /^\/+|\/+$/g;
+			// Cached regex for detecting MSIE.
+			var isExplorer = /msie [\w.]+/;
+			// Cached regex for removing a trailing slash.
+			var trailingSlash = /\/$/;
+			// Cached regex for stripping urls of hash.
+			var pathStripper = /#.*$/;
+
+			// Has the history handling already been started?
+			History.started = false;
+
+			// Set up all inheritable **Backbone.History** properties and methods.
+			//History.prototype
+
+			// The default interval to poll for hash changes, if necessary, is
+			// twenty times a second.
+			History.prototype.interval = 50;
+
+			// Are we at the app root?
+			History.prototype.atRoot = function () {
+				return this.location.pathname.replace(/[^\/]$/, '$&/') === this.root;
+			};
+
+			// Gets the true hash value. Cannot use location.hash directly due to bug
+			// in Firefox where location.hash will always be decoded.
+			History.prototype.getHash = function (window) {
+				var match = (window || this).location.href.match(/#(.*)$/);
+				return match ? match[1] : '';
+			};
+
+			// Get the cross-browser normalized URL fragment, either from the URL,
+			// the hash, or the override.
+			History.prototype.getFragment = function (fragment, forcePushState) {
+				if (fragment == null) {
+					if (this._hasPushState || !this._wantsHashChange || forcePushState) {
+						fragment = decodeURI(this.location.pathname + this.location.search);
+						var root = this.root.replace(trailingSlash, '');
+						if (!fragment.indexOf(root)) fragment = fragment.slice(root.length);
+					} else {
+						fragment = this.getHash();
+					}
+				}
+				return fragment.replace(routeStripper, '');
+			};
+
+			// Start the hash change handling, returning `true` if the current URL matches
+			// an existing route, and `false` otherwise.
+			History.prototype.start = function (options) {
+				if (History.started) throw new Error("history has already been started");
+				History.started = true;
+
+				// Figure out the initial configuration. Do we need an iframe?
+				// Is pushState desired ... is it available?
+				this.options = options == undefined ? {} : options;
+				this.options.root = "/";
+
+				this.path = this.options.path == undefined ? "url" : path;
+				// _.extend({root: '/'}, this.options, options);
+				this.root = this.options.root;
+				this._wantsHashChange = this.options.hashChange !== false;
+				this._wantsPushState = !! this.options.pushState;
+				this._hasPushState = !! (this.options.pushState && this.history && this.history.pushState);
+				var fragment = this.getFragment();
+				var docMode = document.documentMode;
+				var oldIE = (isExplorer.exec(navigator.userAgent.toLowerCase()) && (!docMode || docMode <= 7));
+
+				// Normalize root to always include a leading and trailing slash.
+				this.root = ('/' + this.root + '/').replace(rootStripper, '/');
+
+				if (oldIE && this._wantsHashChange) {
+					var frame = $('<iframe src="javascript:0" tabindex="-1">');
+					this.iframe = frame.hide().appendTo('body')[0].contentWindow;
+					this.navigate(fragment);
+				}
+
+				// Depending on whether we're using pushState or hashes, and whether
+				// 'onhashchange' is supported, determine how we check the URL state.
+				if (this._hasPushState) {
+					$(window).on('popstate', this.checkUrl);
+				} else if (this._wantsHashChange && ('onhashchange' in window) && !oldIE) {
+					$(window).on('hashchange', this.checkUrl);
+				} else if (this._wantsHashChange) {
+					this._checkUrlInterval = setInterval(this.checkUrl, this.interval);
+				}
+
+				// Determine if we need to change the base url, for a pushState link
+				// opened by a non-pushState browser.
+				this.fragment = fragment;
+				var loc = this.location;
+
+				// Transition from hashChange to pushState or vice versa if both are
+				// requested.
+				if (this._wantsHashChange && this._wantsPushState) {
+
+					// If we've started off with a route from a `pushState`-enabled
+					// browser, but we're currently in a browser that doesn't support it...
+					if (!this._hasPushState && !this.atRoot()) {
+						this.fragment = this.getFragment(null, true);
+						this.location.replace(this.root + '#' + this.fragment);
+						// Return immediately as browser will do redirect to new url
+						return true;
+
+						// Or if we've started out with a hash-based route, but we're currently
+						// in a browser where it could be `pushState`-based instead...
+					} else if (this._hasPushState && this.atRoot() && loc.hash) {
+						this.fragment = this.getHash().replace(routeStripper, '');
+						this.history.replaceState({}, document.title, this.root + this.fragment);
+					}
+
+				}
+				// my extension to the startMethod to update the Links, 
+				// and let them not navigate, 
+				// but update the the history
+				$(document).on("click", "a[href^='/']", function(event){
+					var href = $(event.currentTarget).attr('href')
+
+					//# chain 'or's for other black list routes
+					passThrough = href.indexOf('sign_out') >= 0
+					//# Allow shift+click for new tabs, etc.
+					if(!passThrough && !event.altKey && !event.ctrlKey && !event.metaKey && !event.shiftKey){
+							event.preventDefault()
+							//# Remove leading slashes and hash bangs (backward compatablility)
+							url = href.replace(/^\//,'').replace('\#\!\/','')
+							//# Instruct Backbone to trigger routing events
+							 tobserver.utils.history.navigate(url, { trigger: true });
+							return false;
+						}
+					}
+				);
+				
+				if (!this.options.silent) return this.loadUrl();
+			}
+
+			// Disable Backbone.history, perhaps temporarily. Not useful in a real app,
+			// but possibly useful for unit testing Routers.
+			History.prototype.stop = function () {
+				$(window).off('popstate', this.checkUrl).off('hashchange', this.checkUrl);
+				if (this._checkUrlInterval) clearInterval(this._checkUrlInterval);
+				History.started = false;
+			};
+
+			// Add a route to be tested when the fragment changes. Routes added later
+			// may override previous routes.
+			History.prototype.route = function (route, callback) {
+				this.handlers.unshift({
+					route: route,
+					callback: callback
+				});
+			};
+
+			// Checks the current URL to see if it has changed, and if it has,
+			// calls `loadUrl`, normalizing across the hidden iframe.
+			History.prototype.checkUrl = function (e) {
+				var current = this.getFragment();
+				if (current === this.fragment && this.iframe) {
+					current = this.getFragment(this.getHash(this.iframe));
+				}
+				if (current === this.fragment) return false;
+				this.navigate(current);
+				if (this.iframe) this.navigate(current);
+				this.loadUrl();
+			};
+
+			// Attempt to load the current URL fragment. If a route succeeds with a
+			// match, returns `true`. If no defined routes matches the fragment,
+			// returns `false`.
+			History.prototype.loadUrl = function (fragment) {
+				fragment = this.fragment = this.getFragment(fragment);
+				tobserver.set(this.path, fragment);
+			};
+			
+			// Save a fragment into the hash history, or replace the URL state if the
+			// 'replace' option is passed. You are responsible for properly URL-encoding
+			// the fragment in advance.
+			//
+			// The options object can contain `trigger: true` if you wish to have the
+			// route callback be fired (not usually desirable), or `replace: true`, if
+			// you wish to modify the current URL without adding an entry to the history.
+			History.prototype.navigate = function (fragment, options) {
+				if (!History.started) return false;
+				if (!options || options === true) options = {trigger: !! options};
+
+				var url = this.root + (fragment = this.getFragment(fragment || ''));
+				// Strip the hash for matching.
+				fragment = fragment.replace(pathStripper, '');
+				if (this.fragment === fragment) return;
+				this.fragment = fragment;
+				// Don't include a trailing slash on the root.
+				if (fragment === '' && url !== '/') url = url.slice(0, -1);
+				// If pushState is available, we use it to set the fragment as a real URL.
+				if (this._hasPushState) {
+					this.history[options.replace ? 'replaceState' : 'pushState']({}, document.title, url);
+					// If hash changes haven't been explicitly disabled, update the hash
+					// fragment to store history.
+				} else if (this._wantsHashChange) {
+					this._updateHash(this.location, fragment, options.replace);
+					if (this.iframe && (fragment !== this.getFragment(this.getHash(this.iframe)))) {
+						// Opening and closing the iframe tricks IE7 and earlier to push a
+						// history entry on hash-tag change.  When replace is true, we don't
+						// want this.
+						if (!options.replace) this.iframe.document.open().close();
+						this._updateHash(this.iframe.location, fragment, options.replace);
+					}
+					// If you've told us that you explicitly don't want fallback hashchange-
+					// based history, then `navigate` becomes a page refresh.
+				}else
+					return this.location.assign(url);
+				if (options.trigger) return this.loadUrl(fragment);
+			};
+
+			// Update the hash location, either replacing the current entry, or adding
+			// a new one to the browser history.
+			History.prototype._updateHash = function (location, fragment, replace) {
+				if (replace) {
+					var href = location.href.replace(/(javascript:|#).*$/, '');
+					location.replace(href + '#' + fragment);
+				} else {
+					// Some browsers require that `hash` contains a leading #.
+					location.hash = '#' + fragment;
+				}
+			}
+		// Create the default Backbone.history.
+			return new History();
+		}(),
+		bindEvent:function bindEvent(el, eventName, eventHandler) {
+			if (el.addEventListener)
+				el.addEventListener(eventName, eventHandler, false); 
+			else if (el.attachEvent)
+				el.attachEvent('on'+eventName, eventHandler);
 		}
 	};
 	
-	return tobservable;
-})();
-/**
- * a helper function that checks if the nodeelement has a given class
- * @param {type} elem
- *      the dom-Element
- * @param {String} klass
- *      name of the class
- * @returns {Boolean}
- */
-function hasClass( elem, klass ) {
-     return (" " + elem.className + " " ).indexOf( " "+klass+" " ) > -1;
-}
+	/**
+	 * merges a array of names to a path
+	 * @private
+	 * @param {Array} array
+	 *      an array containing strings with the names
+	 * @returns {String}
+	 */
+	var mergeToPath=function(array) {
+		var out = '';
+		for (var ii = 0; ii < array.length; ii++) 
+			if (out === '') 
+				out += array[ii];
+			else 
+				out += '.' + array[ii];
+		return out;
+	}
+	/**
+	 * a helper function that checks if the nodeelement has a given class
+	 * @param {type} elem
+	 *      the dom-Element
+	 * @param {String} klass
+	 *      name of the class
+	 * @returns {Boolean}
+	 */
+	function hasClass( elem, klass ) {
+		 return (" " + elem.className + " " ).indexOf( " "+klass+" " ) > -1;
+	}
 
-/**
- * merges a array of names to a path
- * @private
- * @param {Array} array
- *      an array containing strings with the names
- * @returns {String}
- */
-var mergeToPath=function(array) {
-	var out = '';
-	for (var ii = 0; ii < array.length; ii++) 
-		if (out === '') 
-			out += array[ii];
-		else 
-			out += '.' + array[ii];
-	return out;
-}
-var tobserver = new tobservable(window); ;
-//window.addEventListener("load",function(){
-//	tobserver.initDomViews();
-//});
-function bindEvent(el, eventName, eventHandler) {
-  if (el.addEventListener){
-    el.addEventListener(eventName, eventHandler, false); 
-  } else if (el.attachEvent){
-    el.attachEvent('on'+eventName, eventHandler);
-  }
-}
-// ...
-bindEvent(window, 'load', function(){
+	return new tobservable(window);
+})();
+
+tobserver.utils.bindEvent(window, 'load', function(){
 	var style=document.createElement("style");
 	style.innerHTML=".tobserverlistitem{margin:0px;	padding:0px;}";
 	document.getElementsByTagName("head")[0].appendChild(style);
